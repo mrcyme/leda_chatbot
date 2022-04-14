655ccab3ddf9dc1c4d8725b1ec4abe0e0639f6ec
simeon <simeonmcl@gmail.com>
2022-04-14 17:45:00 +0200

    final test version

--- actions/actions.py
+++ actions/actions.py
@@ -1,34 +1,47 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, FollowupAction
-import pandas as pd
-import actions.search_engine_api_wrapper
+from rasa_sdk.events import SlotSet, FollowupAction, Form
 import json
-
-FEATURE_QUESTIONS = pd.read_csv("data/questions/feature_questions.csv")
-OPEN_QUESTIONS = pd.read_csv("data/questions/open_questions.csv")
-with open("data/sector_presentation.json", "r") as f:
-    SECTOR_PRESENTATION = json.load(f)
+from actions.search_engine_api_wrapper import get_report, get_search_results, get_questions, get_sector_description, get_bc_attribute_list
+import difflib
 
 
-class ActionAskFeatureQuestion(Action):
+QUESTIONS = get_questions()
+FEATURES = [q["feature"] for q in QUESTIONS]
+
+
+class ActionAskQuestion(Action):
 
     def name(self) -> Text:
-        return "action_ask_feature_question"
+        return "action_ask_question"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        for i in range(len(FEATURE_QUESTIONS)):
-            feature, question, responses, _ = FEATURE_QUESTIONS.iloc[i]
-            if not tracker.get_slot(feature):
+        load_questions()
+        for q in QUESTIONS:
+            if not tracker.slots[q["feature"]]:
+                question = q
                 break
-        responses = responses.split(",")
-        data = [{"label": r, "value": r} for r in responses]
-        message = {"payload": "dropDown", "data": data}
-        dispatcher.utter_message(text=question, json_message=message)
-        return [SlotSet(key="active_feature", value=feature)]
+        if question["type"] == "open":
+            dispatcher.utter_message(text=question["question"])
+        else:
+            if question["adapt_on_sector"]:
+                data = [{"label": r, "value": r}
+                        for r in get_bc_attribute_list(q["feature"], filter={"attribute": "sector", "value": tracker.slots["sector"]})]
+            else:
+                data = [{"label": r, "value": r}
+                        for r in question["responses"]]
+            if question["allow_unknown"]:
+                data.append({"label": "I don't know", "value": "Unknown"})
+            if question["type"] == "multiple_choice":
+                message = {"payload": "multiDropDown", "data": data}
+            elif question["type"] == "single_choice":
+                message = {"payload": "dropDown", "data": data}
+            dispatcher.utter_message(
+                text=question["question"], json_message=message)
+        return [SlotSet(key="active_feature", value=question["feature"])]
 
 
 class ActionParseResponse(Action):
@@ -40,37 +53,14 @@ class ActionParseResponse(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         response = tracker.latest_message["text"]
-        feature = tracker.get_slot("active_feature")
+        feature = tracker.slots["active_feature"]
         tracker.slots[feature] = response
-        update_research_state(tracker)
-        return [SlotSet(key=feature, value=response), SlotSet("active_feature", value=None)]
-
-
-class ActionAskOpenQuestion(Action):
-
-    def name(self) -> Text:
-        return "action_ask_open_question"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        for i in range(len(OPEN_QUESTIONS)):
-            if not tracker.get_slot(OPEN_QUESTIONS.iloc[i]["subject"]):
-                feature, question = OPEN_QUESTIONS.iloc[i]
-                break
-        dispatcher.utter_message(text=question)
-        return [SlotSet(key="active_feature", value=feature)]
-
-
-class ActionFetchResults(Action):
-
-    def name(self):
-        return "action_fetch_results"
-
-    def run(self, dispatcher, tracker, domain):
-        results = get_current_results(tracker)
-        print(results)
-        return []
+        if all([tracker.slots[q["feature"]] for q in QUESTIONS]):
+            return [SlotSet(key=feature, value=response), SlotSet("active_feature", value=None), FollowupAction(name="action_present_results")]
+        message = {"payload": "visualization_content",
+                   "data": json.dumps(get_visualization_content(tracker))}
+        dispatcher.utter_message(json_message=message)
+        return [SlotSet(key=feature, value=response), SlotSet("active_feature", value=None), FollowupAction(name="action_ask_question")]
 
 
 class ActionFlushSearchState(Action):
@@ -79,12 +69,9 @@ class ActionFlushSearchState(Action):
         return "action_flush_search_state"
 
     def run(self, dispatcher, tracker, domain):
-        slots_to_clear = list(
-            OPEN_QUESTIONS["subject"].values) + list(FEATURE_QUESTIONS["feature"].values)
-        with open("actions/research_state.json", "w") as f:
-            f.write(json.dumps({"results": ""}))
-        dispatcher.utter_message(text="Sure the search space has been reset")
-        return [SlotSet(key=slot, value=None) for slot in slots_to_clear]
+        load_questions()
+        dispatcher.utter_message(text="The search space has been reset")
+        return [SlotSet(key=slot, value=None) for slot in FEATURES]
 
 
 class ActionRectify(Action):
@@ -92,32 +79,80 @@ class ActionRectify(Action):
         return "action_rectify"
 
     def run(self, dispatcher, tracker, domain):
-        feature = tracker.latest_message["entities"][0]["value"]
-        return [SlotSet(key=feature, value=None), FollowupAction(name="action_ask_feature_question")]
+        try:
+            feature = tracker.latest_message["entities"][0]["value"]
+            feature = search_feature(feature, FEATURES)
+            return [SlotSet(key=feature, value=None), FollowupAction(name="action_ask_question")]
+        except IndexError:
+            dispatcher.utter_message(
+                text="I did not understand the feature you want to correct, could you please reformulate ?")
+
+
+class ActionPresentResults(Action):
+    def name(self):
+        return "action_present_results"
+
+    def run(self, dispatcher, tracker, domain):
+        top_business_cases = get_current_results(tracker)[0:3]
+        url = get_report(tracker.slots, top_business_cases)
+        data = {"payload": "pdf_attachment",
+                "title": "Reports", "url": url}
+        dispatcher.utter_message(json_message=data)
+        dispatcher.utter_message(
+            text="Here is the generated report")
+        return [FollowupAction(name="utter_ask_send_email")]
+
+
+class ActionBack(Action):
+    def name(self):
+        return "action_go_back"
+
+    def run(self, dispatcher, tracker, domain):
+        for e in tracker.events:
+            print(e['event'])
+        print(tracker.events[-6])
+        return []
+
+
+class ActionSendResults(Action):
+    def name(self):
+        return "action_send_results"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(
+            text=f"The report was sent at {tracker.slots['email']}")
+        return [FollowupAction(name="utter_restart")]
 
 
 def get_current_results(tracker):
-    open_knowledge = {k: tracker.slots[k]
-                      for k in OPEN_QUESTIONS["subject"].values if tracker.slots[k] is not None}
+    open_knowledge = {q["feature"]: tracker.slots[q["feature"]]
+                      for q in QUESTIONS if tracker.slots[q["feature"]] is not None and q["type"] == "open"}
 
-    feature_knowledge = {k: tracker.slots[k]
-                         for k in FEATURE_QUESTIONS["feature"].values if tracker.slots[k] is not None}
-    query = " ".join(list(open_knowledge.values())) + \
+    feature_knowledge = {q["feature"]: tracker.slots[q["feature"]]
+                         for q in QUESTIONS if tracker.slots[q["feature"]] is not None and tracker.slots[q["feature"]] != "Unknown" and q["type"] in ["multiple_choice", "single_choice"]}
+    query = " ".join(list(open_knowledge.values())) + " " +\
         " ".join(list(feature_knowledge.values()))
-    return actions.search_engine_api_wrapper.get(feature_knowledge, query=query)
+    return get_search_results(feature_knowledge, query=query)
 
 
-def update_research_state(tracker):
+def get_visualization_content(tracker):
+    results = {}
     if tracker.slots["active_feature"] == "sector":
-        with open("actions/research_state.json", "r") as f:
-            json_decoded = json.load(f)
-            json_decoded["sector_presentation"] = SECTOR_PRESENTATION[tracker.slots["sector"]]
-        with open("actions/research_state.json", "w") as f:
-            f.write(json.dumps(json_decoded))
-    else:
-        results = get_current_results(tracker)
-        with open("actions/research_state.json", "r") as f:
-            json_decoded = json.load(f)
-            json_decoded["results"] = results
-        with open("actions/research_state.json", "w") as f:
-            f.write(json.dumps(json_decoded))
+        sector_description = get_sector_description(
+            tracker.slots["sector"])
+        results["sector_description"] = {
+            "sector": tracker.slots["sector"], "description": sector_description}
+    results["business_cases"] = get_current_results(tracker)
+    return results
+
+
+def search_feature(feature_name, feature_list):
+    return difflib.get_close_matches(feature_name, feature_list, n=1)[0]
+
+
+def load_questions():
+    global QUESTIONS
+    global FEATURES
+    if not QUESTIONS:
+        QUESTIONS = get_questions()
+        FEATURES = [q["feature"] for q in QUESTIONS]
--- actions/search_engine_api_wrapper.py
+++ actions/search_engine_api_wrapper.py
@@ -1,14 +1,42 @@
 import requests
 import json
 
-SEARCH_ENGINE_API_BASE_URL = "http://127.0.0.1:5000/"
+SEARCH_ENGINE_API_BASE_URL = "http://127.0.0.1:5000"
 
 
-def get(features, query=None):
+def get_search_results(features, query=None):
     url = f"{SEARCH_ENGINE_API_BASE_URL}/search"
     data = json.dumps({"query": query, "features": features})
     resp = requests.get(url, json=data)
     return resp.json()['data']
 
 
-#print(get("I work in healthcare, sensor data weelchair",{"sector": "healthcare"}))
+def get_report(features, business_cases):
+    url = f"{SEARCH_ENGINE_API_BASE_URL}/generate_report"
+    data = json.dumps({"features": features, "business_cases": business_cases})
+    resp = requests.get(url, json=data)
+    report_url = f"{SEARCH_ENGINE_API_BASE_URL}/{resp.json()['data']}"
+    return report_url
+
+
+def get_questions():
+    url = f"{SEARCH_ENGINE_API_BASE_URL}/get_questions"
+    try:
+        resp = requests.get(url).json()['data']
+    except requests.exceptions.ConnectionError:
+        print("---------> !!!! Search Engine API is down")
+        resp = []
+    return resp
+
+
+def get_sector_description(sector):
+    url = f"{SEARCH_ENGINE_API_BASE_URL}/get_sector_description/{sector}"
+    resp = requests.get(url)
+    return resp.json()['data']
+
+
+def get_bc_attribute_list(attribute, filter=None):
+    url = f"{SEARCH_ENGINE_API_BASE_URL}/get_bc_attribute_list/{attribute}"
+    data = json.dumps({"filter": filter})
+    resp = requests.get(url, json=data)
+    return resp.json()['data']
